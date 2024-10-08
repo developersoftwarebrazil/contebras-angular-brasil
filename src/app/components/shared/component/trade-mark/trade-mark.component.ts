4844fd18e62a965020dc822943ed635d31c16bd3
dnadocodigo <dnadevnet@gmail.com>
2024-10-08 16:31:08 -0300

    criado o component-trade-mark  para o logo

---
+++ src/app/components/shared/component/trade-mark/trade-mark.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trade-mark',
+  standalone: true,
+  imports: [],
+  templateUrl: './trade-mark.component.html',
+  styleUrl: './trade-mark.component.scss'
+})
+export class TradeMarkComponent {
+
+}
